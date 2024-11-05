44e607dfd90bfa0ddddff120a9b07368f132cb75
Kavi17498 <kavindulakshan187@gmail.com>
2024-11-05 23:22:17 +0530

    text Controller Added

--- lib/widgets/addnewexpences.dart
+++ lib/widgets/addnewexpences.dart
@@ -8,12 +8,21 @@ class Addnewexpences extends StatefulWidget {
 }
 
 class _AddnewexpencesState extends State<Addnewexpences> {
+  final _titleController = TextEditingController();
+  @override
+  void dispose() {
+    // TODO: implement dispose
+
+    _titleController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
         //title text Filed
         TextField(
+          controller: _titleController,
           decoration:
               InputDecoration(hintText: "Add New Title", label: Text("Title")),
           keyboardType: TextInputType.text,
